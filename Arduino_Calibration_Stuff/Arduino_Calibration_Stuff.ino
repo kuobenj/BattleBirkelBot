796cb33052fed5691de72381bdca8bd5e8457c5e
Matt Birkel <mbirkel91@gmail.com>
2018-02-28 20:54:58 -0500

    Quick cleanup of Arduino_Calibration_Stuff

--- Arduino_Calibration_Stuff/Arduino_Calibration_Stuff.ino
+++ Arduino_Calibration_Stuff/Arduino_Calibration_Stuff.ino
@@ -5,20 +5,21 @@ long num;
 
 void setup()
 {
- servo1.attach(9);
-Serial.begin(9600); 
-Serial.print("Enter Position = ");
+  servo1.attach(9);
+  Serial.begin(9600); 
+  Serial.print("Enter Position = ");
 }
 
 void loop() 
 { 
-  while(Serial.available()>0)
-  { 
-  num= Serial.parseInt();   
-  Serial.print(num);  
-  Serial.println(" degree");
-  Serial.print("Enter Position = ");
+  while(Serial.available() > 0)
+  {
+    num = Serial.parseInt();   
+    Serial.print(num);  
+    Serial.println(" degree");
+    Serial.print("Enter Position = ");
   }
   servo1.write(num);
   delay(15);
 }
+
